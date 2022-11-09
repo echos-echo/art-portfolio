9a9d8c7bdf00ab9f5de8d220d422c1ac778887ce
Eve Cho <e.young.echo@gmail.com>
2022-11-09 16:37:13 -0500

    feature: component for uploading images created

---
+++ components/Upload.js
@@ -0,0 +1,10 @@
+// this will be the component where you are able to upload more images
+// should be password access only (no need to encrypt; personal use only)
+// a form that takes in
+// - image file (jpg, jpeg, png)
+// - image type (sketch, comic, illustration, etc)
+// - image tags
+
+export default function Upload() {
+    return (<></>);
+}
